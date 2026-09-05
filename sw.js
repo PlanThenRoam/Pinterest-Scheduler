201a196bf0da48c15265f25d873a8e50479465f0
PlanThenRoam <maabdullah175@gmail.com>
2026-09-05 21:08:13 +0100

    Refresh app cache for version 12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ptr-seller-tools-v11';
+const CACHE_NAME='ptr-seller-tools-v12';
 const APP_SHELL=['./','./index.html','./video-studio.html','./video-renderer.html','./oauth-consent.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
