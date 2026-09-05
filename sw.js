dd7cb18a13e484c20c4ae3e95f84db077c0ae791
PlanThenRoam <maabdullah175@gmail.com>
2026-09-05 20:56:09 +0100

    Release Seller Tools version 10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ptr-seller-tools-v9';
+const CACHE_NAME='ptr-seller-tools-v10';
 const APP_SHELL=['./','./index.html','./video-studio.html','./video-renderer.html','./oauth-consent.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
